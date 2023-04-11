92cc9b68c0d5dea883d3829ec220a482b66fff06
Pritesh shakya <123233989+pritesh-analyst@users.noreply.github.com>
2023-04-11 16:17:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -587,7 +587,7 @@ def Getdatafornamemistakes(date):
     return df_filtered
 
 
-def Getdata_laptop_entries():
+def Getdataentries(date):
     import pandas as pd
     import datetime as dt
     from pandas import Timestamp
@@ -629,12 +629,12 @@ def Getdata_laptop_entries():
     df1["Timestamp"] = pd.to_datetime(df1["Timestamp"])
     df1['DATE'] = pd.to_datetime(df1['DATE'])
 
-    g4_swappings = print("Total G4 Swaappings :",data[ (data['Center'] == 'G4') & (data['Date'] == '2023-04-11')].shape[0])
-    laptop_entries = print("Total Laptop Entries :",df1.loc[(df1['DATE'] == '2023-04-11') & df1['Battery Number SL (only write number, please do not write SL)']]['Battery Number SL (only write number, please do not write SL)'].count())
+    g4_swappings = print("Total G4 Swaappings :",data[ (data['Center'] == 'G4') & (data['Date'] == date)].shape[0])
+    laptop_entries = print("Total Laptop Entries :",df1.loc[(df1['DATE'] == date) & df1['Battery Number SL (only write number, please do not write SL)']]['Battery Number SL (only write number, please do not write SL)'].count())
 
     
         
-    return g4_swappings,laptop_entries    
+    return g4_swappings,laptop_entries
     
 
 
@@ -650,7 +650,7 @@ def main():
         sheet_data = "Form Responses 1"
         date = str(st.sidebar.date_input("Select start date"))
         data = load_data_daywise1(sheet_id, sheet_data,date)
-        laptop_entries = Getdata_laptop_entries()
+        laptop_entries = Getdataentries(date)
 
         st.title("Battery Out")
             # st.write("This app shows Hourly battery out today.")
